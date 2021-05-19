1aa727a94b41ac51c8d12f155afdf006a70976bc
sankeerthreddyalleti <71555571+sankeerthreddyalleti@users.noreply.github.com>
2021-05-19 16:35:17 -0400

    Add files via upload

---
+++ ADB project/main.py
@@ -0,0 +1,95 @@
+print(f'Postgres\n{"="*8}')
+
+print(f'Storage (MB): 775')
+
+print(f'Total write time (s): 15.98')
+
+print(f'Total read time (s): 0.62')
+
+print(f'Total append 1-record time (s): 0.12')
+
+print(f'Total append 1000-record time (s): 2.69')
+
+print(f'Total delete time (s): 0.66')
+
+print()
+
+print(f'PostgreJSON\n{"="*11}')
+
+print(f'Storage (MB): 150')
+
+print(f'Total write time (s): 5.32')
+
+print(f'Total read time (s): 0.81')
+
+print(f'Total append 1-record time (s): 3.08')
+
+print(f'Total append 1000-record time (s): 4.61')
+
+print(f'Total delete time (s): 0.01')
+
+print()
+
+print(f'PostgresJSONB\n{"="*12}')
+
+print(f'Storage (MB): 174')
+
+print(f'Total write time (s): 6.46')
+
+print(f'Total read time (s): 1.19')
+
+print(f'Total append 1-record time (s): 2.06')
+
+print(f'Total append 1000-record time (s): 2.87')
+
+print(f'Total delete time (s): 0.02')
+
+print()
+
+print(f'TimescaleDB\n{"="*11}')
+
+print(f'Storage (MB): 735')
+
+print(f'Total write time (s): 20.16')
+
+print(f'Total read time (s): 0.66')
+
+print(f'Total append 1-record time (s): 0.14')
+
+print(f'Total append 1000-record time (s): 3.46')
+
+print(f'Total delete time (s): 1.01')
+
+print()
+
+print(f'MongoDB\n{"="*7}')
+
+print(f'Storage (MB): 209')
+
+print(f'Total write time (s): 3.03')
+
+print(f'Total read time (s): 0.37')
+
+print(f'Total append 1-record time (s): 0.87')
+
+print(f'Total append 1000-record time (s): 1.58')
+
+print(f'Total delete time (s): 0.09')
+
+print()
+
+print(f'Arctic\n{"="*6}')
+
+print(f'Storage (MB): 195')
+
+print(f'Total write time (s): 1.12')
+
+print(f'Total read time (s): 0.44')
+
+print(f'Total append 1-record time (s): 0.35')
+
+print(f'Total append 1000-record time (s): 0.57')
+
+print(f'Total delete time (s): 1.20')
+
+print()
